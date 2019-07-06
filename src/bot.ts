271556679315ffb920f749cef454ac9aa3c6b82d
miguelc <miguel.coelho@thetrainline.com>
2019-07-06 16:26:58 +0100

    Removed ts-node

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
 /// <reference path="./global.d.ts" />
 import * as Discord from 'discord.io';
 
-import auth from '../auth';
+import auth from './auth';
 import { onMessage, onReady } from './bot-config/bot-config';
 
 const bot = new Discord.Client({
